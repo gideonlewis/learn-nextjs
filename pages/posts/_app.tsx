f73e177d5ed88eeac69b769e296851e120758be8
quanhuynh <quanhuynh@teqnological.asia>
2023-12-12 11:27:09 +0700

    implement routes testing

---
+++ pages/posts/_app.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface IAppProps {}
+
+export default function App(props: IAppProps) {
+  return (
+    <div>
+      <h1>The _app.tsx</h1>
+    </div>
+  );
+}
---
+++ pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface IAppProps {}
+
+export default function App(props: IAppProps) {
+  return (
+    <div>
+      <h1 className="container">The index route testing</h1>
+    </div>
+  );
+}
